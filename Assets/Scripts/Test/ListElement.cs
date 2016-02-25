445770778345627d4e1022a53b0af4164ab9533e
Ragzouken <ragzouken@gmail.com>
2016-02-25 16:30:40 +0000

    testing lists

---
+++ Assets/Scripts/Test/ListElement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ListElement : MonoBehaviour 
+{
+    [SerializeField] private Toggle toggle;
+    [SerializeField] private GameObject selectedObject;
+    [SerializeField] private GameObject highlightedObject;
+
+    [SerializeField] private Text textPrefab;
+    [SerializeField] private RectTransform textContainer;
+
+    public bool highlighted
+    {
+        set
+        {
+            highlightedObject.SetActive(value);
+        }
+    }
+
+    private MonoBehaviourPooler<string, Text> labels;
+
+    private Action select;
+
+    private void Awake()
+    {
+        labels = new MonoBehaviourPooler<string, Text>(textPrefab,
+                                                       textContainer,
+                                                       (text, label) => label.text = text);
+
+        toggle.onValueChanged.AddListener(OnToggled);
+    }
+
+    public void Setup(Action action, params string[] labels)
+    {
+        this.select = action;
+        this.labels.SetActive(labels);
+    }
+
+    private void OnToggled(bool active)
+    {
+        select();
+    }
+}
--- Assets/Scripts/Test/Test.cs
+++ Assets/Scripts/Test/Test.cs
@@ -7,4 +7,33 @@ using System.Collections.Generic;
 
 public class Test : MonoBehaviour 
 {
+    [SerializeField] private ListElement elementPrefab;
+    [SerializeField] private RectTransform attributesContainer;
+
+    private MonoBehaviourPooler<DAttribute, ListElement> attributes;
+    
+    private void Awake()
+    {
+        attributes = new MonoBehaviourPooler<DAttribute, ListElement>(elementPrefab,
+                                                                      attributesContainer,
+                                                                      InitialiseAttribute);
+    }
+
+    private void InitialiseAttribute(DAttribute attribute,
+                                     ListElement element)
+    {
+        element.Setup(delegate { },
+                      attribute.name,
+                      attribute.type.ToString());
+    }
+
+    private void Start()
+    {
+        attributes.SetActive(new[]
+        {
+            new DAttribute { name = "mobile", type = DType.Tag        },
+            new DAttribute { name = "range",  type = DType.Value      },
+            new DAttribute { name = "owner",  type = DType.Collection },
+        });
+    }
 }
