affe747d78c20457215796fec3a1dd15450f3258
Ragzouken <ragzouken@gmail.com>
2016-02-25 15:13:48 +0000

    towards test skeleton

---
+++ Assets/Scripts/Model/Game.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Assertions;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum DType
+{
+    Tag,
+    State,
+    Value,
+    Collection,
+}
+
+public class Game
+{
+    public State state;
+    public Rules rules;
+}
+
+public class Rules
+{
+    public ICollection<DAttribute> attributes;
+}
+
+public class State
+{
+    public ICollection<DObject> objects;
+}
+
+public class DObject
+{
+    public Dictionary<DAttribute, object> attributes;
+}
+
+public class DAttribute
+{
+    public string name;
+    public DType type;
+}
