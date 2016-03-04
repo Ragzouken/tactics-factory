1ae1bc45711184aa4b91cbb269da92929f546c39
Ragzouken <ragzouken@gmail.com>
2016-03-04 16:57:37 +0000

    hover insert

--- Assets/Scripts/Test/TestEditor.cs
+++ Assets/Scripts/Test/TestEditor.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
 using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,6 +12,9 @@ public class TestEditor : MonoBehaviour
     [SerializeField] private LineElement signature;
     [SerializeField] private RectTransform lineContainer;
     [SerializeField] private LineElement linePrefab;
+    [SerializeField] private Button addLineButton;
+    [SerializeField] private Button insertButton;
+    [SerializeField] private RectTransform gutter;
 
     private MonoBehaviourPooler<AST.Line, LineElement> lines;
 
@@ -19,6 +23,11 @@ public class TestEditor : MonoBehaviour
         lines = new MonoBehaviourPooler<AST.Line, LineElement>(linePrefab,
                                                                lineContainer,
                                                                (l, e) => e.Setup(l));
+
+        addLineButton.onClick.AddListener(AddLine);
+        insertButton.onClick.AddListener(InsertLine);
+
+        insertButton.gameObject.SetActive(false);
     }
 
     private AST.Function function;
@@ -113,12 +122,58 @@ public class TestEditor : MonoBehaviour
         SetFunction(function);
     }
 
+    private AST.Line hoveredLine;
+    private bool hoveredAbove;
+
+    private void Update()
+    {
+        Vector2 point;
+
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(gutter, Input.mousePosition, null, out point);
+        bool show = gutter.rect.Contains(point);
+
+        hoveredLine = null;
+
+        foreach (var line in lines.Shortcuts)
+        { 
+            var rtrans = lines.Get(line).transform as RectTransform;
+
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(rtrans, Input.mousePosition, null, out point);
+
+            var rect = rtrans.rect;
+            rect.height += 4;
+
+            bool inside = rect.Contains(point);
+
+            if (inside)
+            {
+                bool above = point.y > 0;
+
+                Vector3 pos = insertButton.transform.position;
+                pos.y = rtrans.position.y + (above ? 14 : -14);
+                insertButton.transform.position = pos;
+
+                hoveredLine = line;
+                hoveredAbove = above;
+
+                break;
+            }
+        }
+
+        Debug.LogFormat("Hovered Line {0} Above? {1}", hoveredLine, hoveredAbove);
+
+        insertButton.gameObject.SetActive(show);
+    }
+
     public void SetFunction(AST.Function function)
     {
         this.function = function;
 
         signature.Setup(function.signature);
         lines.SetActive(function.definition);
+
+        insertButton.transform.SetAsLastSibling();
+        addLineButton.transform.SetAsLastSibling();
     }
 
     private AST.Line dragging;
@@ -140,8 +195,9 @@ public class TestEditor : MonoBehaviour
     public void DeleteLine()
     {
         function.definition.Remove(dragging);
-        lines.SetActive(function.definition);
         EndDrag(dragging);
+
+        SetFunction(function);
     }
 
     public void SwapLine(AST.Line line)
@@ -149,7 +205,57 @@ public class TestEditor : MonoBehaviour
         function.definition.Remove(dragging);
         int index = function.definition.IndexOf(line);
         function.definition.Insert(index + 1, dragging);
-        lines.SetActive(function.definition);
         EndDrag(dragging);
+
+        SetFunction(function);
+    }
+
+    public void AddLine()
+    {
+        function.definition.Add(new AST.Line
+        {
+            arguments = new[]
+                {
+                    new AST.Component("set"),
+                    new AST.Component("next", AST.Type.Position),
+                    new AST.Component("to"),
+                    new AST.Component("the first <i>position</i> in"),
+                    new AST.Component("path", AST.Type.Position, true),
+                },
+        });
+
+        SetFunction(function);
+    }
+
+    public void OnHoverGutterBegin()
+    {
+        insertButton.gameObject.SetActive(true);
+    }
+
+    public void OnHoverGutterEnd()
+    {
+        insertButton.gameObject.SetActive(false);
+    }
+
+    public void InsertLine()
+    {
+        Assert.IsNotNull(hoveredLine, "No line hovered to insert relative to!");
+
+        var line = new AST.Line
+        {
+            arguments = new[]
+            {
+                new AST.Component("set"),
+                new AST.Component("next", AST.Type.Position),
+                new AST.Component("to"),
+                new AST.Component("the first <i>position</i> in"),
+                new AST.Component("path", AST.Type.Position, true),
+            },
+        };
+
+        int index = function.definition.IndexOf(hoveredLine);
+        function.definition.Insert(index + (hoveredAbove ? 0 : 1), line);
+
+        SetFunction(function);
     }
 }
